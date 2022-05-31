08ed96b994bf7fb957dae6b1251dd91868c14b75
Dan Siegel <me@dansiegel.net>
2022-05-31 11:49:55 -0700

    add tests for sample report

--- SampleLib.Tests/UnitTest1.cs
+++ SampleLib.Tests/UnitTest1.cs
@@ -7,4 +7,16 @@ public class UnitTest1
     {
 
     }
+
+    [Fact]
+    public void FailingTest()
+    {
+        Assert.True(false);
+    }
+
+    [Fact(Skip = "CI Test")]
+    public void TestToSkip()
+    {
+        
+    }
 }

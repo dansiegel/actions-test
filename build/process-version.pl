7d01ffd97c40f97ebc0b1b33c397a9afcb64635c
Dan Siegel <me@dansiegel.net>
2022-06-02 14:09:23 -0700

    remove double quotes

--- build/process-version.pl
+++ build/process-version.pl
@@ -21,16 +21,16 @@ for my $i (@files) {
 
 if($preview eq "") {
     print "::set-output name=is-preview::\"false\"\n";
-    print "::set-output name=release-display-name::\"$version\"\n";
+    print "::set-output name=release-display-name::$version\n";
 } elsif($preview =~ m/alpha/) {
     print "::set-output name=is-preview::\"true\"\n";
-    print "::set-output name=release-display-name::\"$version - Alpha\"\n";
+    print "::set-output name=release-display-name::$version - Alpha\n";
 } elsif($preview =~ m/beta/) {
     print "::set-output name=is-preview::\"true\"\n";
-    print "::set-output name=release-display-name::\"$version - Beta\"\n";
+    print "::set-output name=release-display-name::$version - Beta\n";
 } else {
     print "::set-output name=is-preview::\"true\"\n";
-    print "::set-output name=release-display-name::\"$version - Preview\"\n";
+    print "::set-output name=release-display-name::$version - Preview\n";
 }
 
-print "::set-output name=version-name::\"$gitTag\"\n"
+print "::set-output name=version-name::$gitTag\n"
